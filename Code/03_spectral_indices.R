1f6cb2ba756aef9ff81d2d65493c9aaf932d44c0
gretamarchisotti <marchisottigreta@gmail.com>
2025-03-12 17:48:36 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -21,3 +21,54 @@ im.plotRGB(mato1992, r=1, g=2, b=3) # Tutta la vegetazione diventa rossa
 im.plotRGB(mato1992, r=2, g=1, b=3) # Tutta la vegetazione diventa rossa e il suolo nudo è rosa
 im.plotRGB(mato1992, r=2, g=3, b=1) # Tutta la vegetazione diventa blu e il suolo nudo è giallo, anche l'acqua è gialla perchè ha molti solidi disciolti
 
+# Importiamo l'immagine del 2006 e la invertiamo
+mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 = flip(mato2006)
+plot(mato2006)
+
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+# Osserviamo le due immagini messe a confronto
+im.multiframe(1,2)
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+# Mettiamo in risalto il suolo nudo in giallo
+im.plotRGB(mato1992, r=3, g=2, b=1)
+im.plotRGB(mato2006, r=3, g=2, b=1)
+
+
+im.multiframe(3,2) # Inserisco tutte e sei le immagini insieme
+# NIR ontop of red
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+# NIR ontop of green
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato2006, r=2, g=1, b=3)
+
+# NIR ontop of blue
+im.plotRGB(mato1992, r=3, g=2, b=1)
+im.plotRGB(mato2006, r=3, g=2, b=1)
+
+# Exercise: plot only the first layer of mato2006
+dev.off()
+plot(mato2006[[1]])
+
+plot(mato2006[[1]], col=magma(100)) # Cambiamo il colore
+plot(mato2006[[1]], col=mako(100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
