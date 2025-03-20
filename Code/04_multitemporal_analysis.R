95210045ba9e52d864f54b240fd1d7eb32f3f479
gretamarchisotti <marchisottigreta@gmail.com>
2025-03-20 14:26:00 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -34,22 +34,3 @@ gr = im.import("greenland")
 grdif = gr[[4]] - gr[[1]] #2015 - 2000
 plot(grdif)
 # Tutte le parti gialle sono quelle che nel 2015 mostrano un aumento di temperatura maggiore rispetto alle temperature del 2000
-
-# ---------------------------------------------------------
-# Esportazione delle immagini da R e importazione in github
-
-# Selezioniamo la cartella di lavoro
-setwd("C:/Users/march/Desktop/BOLOGNA/II semestre/Telerilevamento geoecologico in R")
-
-# Per vedere la cartella selezionata
-getwd()
-
-# Creiamo un file PDF con la funzione pdf()
-pdf("output.pdf") # Creiamo il file
-plot(grdif) # Plottiamo il grafico all'interno del file
-dev.off() # Alla fine devo chiudere il file altrimenti il PDF non viene creato
-
-# Creiamo un file immagine con la funzione jpeg
-jpeg("output.jpeg")
-plot(grdif)
-dev.off()
