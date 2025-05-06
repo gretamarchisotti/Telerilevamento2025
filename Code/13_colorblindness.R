b2e9508bd6c5e136c432adbf45e6b2823db191d9
gretamarchisotti <marchisottigreta@gmail.com>
2025-05-06 15:56:23 +0200

    Create 13_colorblindness.R

---
+++ Code/13_colorblindness.R
@@ -0,0 +1,11 @@
+# Code to solve colorblindness problems
+
+# Packages
+library(terra)
+library(imageRy)
+
+# Installing cblindplot
+library(devtools)
+install_github("ducciorocchini/cblindplot")
+library(cblindplot)
+
