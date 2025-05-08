485fe504f5a72c3d2c1bd455b12d2a0558d8699c
gretamarchisotti <marchisottigreta@gmail.com>
2025-05-08 14:37:03 +0200

    Update 13_colorblindness.R

--- Code/13_colorblindness.R
+++ Code/13_colorblindness.R
@@ -9,3 +9,24 @@ library(devtools)
 install_github("ducciorocchini/cblindplot")
 library(cblindplot)
 
+# Importing data
+setwd("C:\Users\march\Desktop\BOLOGNA\II semestre\Telerilevamento geoecologico in R")
+vinicunca = rast("vinicunca.jpg")
+plot(vinicunca)
+vinicunca = flip(vinicunca)
+plot(vinicunca)
+
+# Simultaing colorblindness
+im.multiframe(2,1)
+im.plotRGB(vinicunca, r=1, g=2, b=3, title="Standard Vision")
+im.plotRGB(vinicunca, r=2, g=1, b=3, title="Protanopia")
+
+# Solving colorblindness
+dev.off()
+rainbow=rast("rainbow.jpg")
+plot(rainbow)
+rainbow = flip(rainbow)
+plot(rainbow)
+cblind.plot(rainbow, cvd="protanopia")
+cblind.plot(rainbow, cvd="deuteranopia")
+cblind.plot(rainbow, cvd="tritanopia")
