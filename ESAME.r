bb87ecaba6510bc86ae82844672f2a3c6eb9a60f
gretamarchisotti <marchisottigreta@gmail.com>
2025-06-30 15:17:47 +0200

    Update ESAME.r

--- ESAME.r
+++ ESAME.r
@@ -1,134 +1,44 @@
+# Progetto d'Esame, Greta Marchisotti - Codice in R per l'elaborazione delle immagini
+# Le immagini sono relative agli incendi verificatisi nella primavera del 2025 in Canada, nell'area a confine tra le regioni Manitoba e Saskatchewan
+# Grazie a Google Earth Engine sono state scaricate due immagini di Sentinel-2 (per il codice vedere lo script Esame.js)
+# La prima riguarda una media delle immagini di giugno 2024, la seconda una media delle immagini di giugno 2025, dopo che si sono verificati gli incendi
+
+# Il salvataggio delle immagini da R, salvo diversamente specificato, è stato fatto con il menù a tendina di R
+
 # Pacchetti richiesti e utilizzati
-library(terra)
-library(imageRy)
+library(terra) # Paccheto per l'analisi spaziale dei dati con vettori e dati raster
+library(imageRy) # Pacchetto per manipolare, visualizzare ed esportare immagini raster in R
+library(viridis) # Pacchetto per cambiare le palette di colori anche per chi è affetto da colorblindness
 
 # Imposto la working directory
 setwd("C:/Users/march/Desktop/BOLOGNA/II semestre/Telerilevamento geoecologico in R/ESAME")
 
-# Importo le immagini
-sentinel2024 <- rast("sentinel2_Canada2024.tif")
+# Importo in R le immagini scaricate con Google Earth Engine
+sentinel2024 <- rast("Canada2024.tif")
 sentinel2024
 
-sentinel2025 <- rast("sentinel2_Canada2025.tif")
+sentinel2025 <- rast("Canada2025.tif")
 sentinel2025
 
-# Visualizzo le immagini in RGB
-plotRGB(sentinel2024, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median) 2024")
-plotRGB(sentinel2025, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel (median) 2025")
-
-# Il salvataggio delle immagini è stato fatto con il menù a tendina di R
-
-# ----------------------------------------------------
-# Visualizzazione dell'immagine con il NIR
-im.multiframe(3,2)
-# NIR ontop of red
-im.plotRGB(mato1992, r=1, g=2, b=3)
-im.plotRGB(mato2006, r=1, g=2, b=3)
-
-# ----------------------------------------------------
-# Calcolo del DVI
-
-# Functions from imageRy
-dvi1992auto = im.dvi(mato1992, 1, 2)
-dev.off()
-plot(dvi1992auto)
-
-dvi2006auto = im.dvi(mato2006, 1, 2)
-dev.off()
-plot(dvi2006auto)
-
-ndvi1992auto = im.ndvi(mato1992, 1, 2)
-dev.off()
-plot(ndvi1992auto)
-
-ndvi2006auto = im.ndvi(mato2006, 1, 2)
-dev.off()
-plot(ndvi2006auto)
-
+# Visualizzo entrambe le immagini in RGB creando un multiframe e le salvo in .png; infine chiudo il pannello grafico
 im.multiframe(1,2)
-plot(ndvi1992)
-plot(ndvi1992auto)
+plotRGB(sentinel2024, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel-2 (median) 2024")
+plotRGB(sentinel2025, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel-2 (median) 2025")
+dev.off()
 
-# -----------------------------------------------------
-# Analisi multitemporale
-gr = im.import("greenland")
+# Visualizzo le quattro bande separate (RGB e NIR) per entrambe le immagini
+plot(sentinel2024, main=c("B4-Red", "B3-Green", "B2-Blue", "B8-NIR"), col=magma(100))
+plot(sentinel2025, main=c("B4-Red", "B3-Green", "B2-Blue", "B8-NIR"), col=magma(100))
 
-im.multiframe(1,2)
-plot(gr[[1]], col=rocket(100))
-plot(gr[[4]], col=rocket(100))
+# Salvo le immagini in formato .png
 
-grdif = gr[[4]] - gr[[1]] # 2015 - 2000
-plot(grdif)
-# All the yellow parts are those in which there is a higher value in 2015
+# Visualizzo le due immagini con il NIR ponendo il NIR al posto del filtro red e le inserisco in un multiframe con le immagini in RGB
+im.multiframe(2,2)
+plotRGB(sentinel2024, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel-2 (median) 2024")
+plotRGB(sentinel2025, r = 1, g = 2, b = 3, stretch = "lin", main = "Sentinel-2 (median) 2025")
+im.plotRGB(sentinel2024, r=4, g=1, b=3)
+im.plotRGB(sentinel2025, r=4, g=1, b=3)
+dev.off() # Chiudo il pannello grafico dopo aver salvato le immagini in .png
 
-# Ridgeline plots
-im.ridgeline(gr, scale=1)
-im.ridgeline(gr, scale=2)
-im.ridgeline(gr, scale=2, palette="inferno")
-im.ridgeline(gr, scale=3, palette="inferno")
 
-# Exercise: import the NDVI data from Sentinel 
-ndvi = im.import("Sentinel2_NDVI")
-im.ridgeline(ndvi, scale=2)
 
-# Changing names
-# sources     : Sentinel2_NDVI_2020-02-21.tif  
-#               Sentinel2_NDVI_2020-05-21.tif  
-#               Sentinel2_NDVI_2020-08-01.tif  
-#               Sentinel2_NDVI_2020-11-27.tif  
-
-names(ndvi) = c("02_Feb", "05_May", "08_Aug", "11_Nov")
-im.ridgeline(ndvi, scale=2)
-im.ridgeline(ndvi, scale=2, palette="mako")
-
-pairs(ndvi)
-
-plot(ndvi[[1]], ndvi[[2]])
-# y = x # may y, feb x
-# y = a + bx
-# a=0, b=1
-# y = a + bx = 0 + 1x = x
-
-abline(0, 1, col="red")
-
-plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3, 0.9))
-abline(0, 1, col="red")
-
-im.multiframe(1,3)
-plot(ndvi[[1]])
-plot(ndvi[[2]])
-plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3, 0.9))
-abline(0, 1, col="red")
-
-# -------------------------------------------------------
-# Classificazione?
-
-# -------------------------------------------------------
-# Deviazione standard
-
-# Exercise: import the NDVI data from Sentinel 
-ndvi = im.import("Sentinel2_NDVI")
-im.ridgeline(ndvi, scale=2)
-
-names(ndvi) = c("02_Feb", "05_May", "08_Aug", "11_Nov")
-im.ridgeline(ndvi, scale=2)
-im.ridgeline(ndvi, scale=2, palette="mako")
-
-pairs(ndvi)
-
-plot(ndvi[[1]], ndvi[[2]])
-# y = x # may y, feb x
-# y = a + bx
-# a=0, b=1
-# y = a + bx = 0 + 1x = x
-
-abline(0, 1, col="red")
-
-plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3, 0.9))
-abline(0, 1, col="red")
-
-im.multiframe(1,3)
-plot(ndvi[[1]])
-plot(ndvi[[2]])
-plot(ndvi[[1]], ndvi[[2]], xlim=c(-0.3,0.9), ylim=c(-0.3, 0.9))
-abline(0, 1, col="red")
